ce7a21a5e7333645deab0ae3ad6a1c8bd42d495a
Savic Prvoslav <savic.prvoslav@gmail.com>
2015-01-07 16:32:04 +0100

    load up to 100 users.

--- src/main/webapp/app/scripts/factory/userService.js
+++ src/main/webapp/app/scripts/factory/userService.js
@@ -1,4 +1,4 @@
 angular.module('activitiApp').factory('UserService', function ($resource) {
-    var data = $resource('/service/identity/users/:user', {user: "@user"});
+    var data = $resource('/service/identity/users/:user', {user: "@user", size: 100});
     return data;
 });
